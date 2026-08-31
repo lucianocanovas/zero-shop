999b1487d68e4195fafca834946fa04ba34ecb48
Luciano Canovas <canovaslucho11@gmail.com>
2026-08-30 21:54:54 -0300

    Frontend

---
+++ src/main/resources/static/scripts/navbar.js
@@ -0,0 +1,7 @@
+navButton = document.querySelector('.nav-button');
+accountButton = document.querySelector('.account-button');
+
+navButton.addEventListener('click', function() {
+    // Aquí puedes agregar la lógica para mostrar u ocultar el menú de navegación
+    alert('Botón de menú clickeado');
+});
