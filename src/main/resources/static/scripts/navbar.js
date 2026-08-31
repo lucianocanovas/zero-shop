c37908d31a1828c4bbb4bdbb92ec7973cf77278c
Luciano Canovas <canovaslucho11@gmail.com>
2026-08-31 17:22:39 -0300

    Frontend

--- src/main/resources/static/scripts/navbar.js
+++ src/main/resources/static/scripts/navbar.js
@@ -1,11 +1,15 @@
-const navButton = document.querySelector('.nav-button');
-const closeButton = document.querySelector('.close-button');
+const navButton = document.querySelector('.header-nav-button');
+const closeButton = document.querySelector('.nav-close-button');
 const navOverlay = document.querySelector('.nav-overlay');
 
-navButton.addEventListener('click', () => {
-    navOverlay.classList.toggle('active');
+navButton.addEventListener("click", () => {
+    navOverlay.classList.toggle("active");
 });
 
-closeButton.addEventListener('click', () => {
-    navOverlay.classList.remove('active');
+closeButton.addEventListener("click", () => {
+    navOverlay.classList.remove("active");
+});
+
+navOverlay.addEventListener("click", () => {
+    navOverlay.classList.remove("active");
 });
